d3ed584e238b857ab11724d5d6d18ab5bf36b96a
TheJamesLJ <jameslj.efc@hotmail.co.uk>
2023-11-07 01:00:27 +0000

    Added wonder trade by Lunos/LOuroboros

--- include/battle_util.h
+++ include/battle_util.h
@@ -250,5 +250,6 @@ bool32 AreBattlersOfOppositeGender(u32 battler1, u32 battler2);
 bool32 AreBattlersOfSameGender(u32 battler1, u32 battler2);
 u32 CalcSecondaryEffectChance(u32 battler, u8 secondaryEffectChance);
 u8 GetBattlerType(u32 battler, u8 typeIndex);
+bool32 DoesSpeciesUseHoldItemToChangeForm(u16 species, u16 heldItemId);
 
 #endif // GUARD_BATTLE_UTIL_H
